a9c73dee306c3dff7bd74236fe6e1428433ca005
cirellapietro <133475613+cirellapietro@users.noreply.github.com>
2025-08-31 21:45:10 +0200

    Create Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="footer">
+    <p>&copy; 2024 Manutenzione Veicoli</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style>
+.footer {
+  background-color: #333;
+  color: white;
+  text-align: center;
+  padding: 1rem;
+  margin-top: auto;
+}
+</style>
